52d190482acf8fdbb24595212234a69f19e8acf8
NatalyaYanochkova <tusyanaletova@yandex.ru>
2022-12-11 19:19:02 +0200

    Added new options

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 /*
 Create function which receives 2 params, compares them and sends alert with
     text ‘a > b’, ‘a<b’, ‘a==b’’ or ‘a===b’.*/
-let fun = function (a, b) {
+function comparatorAB(a, b) {
     if (a > b) {
         alert('a > b');
     } else if (a < b) {
@@ -9,47 +9,67 @@ let fun = function (a, b) {
     } else if (a === b) {
         alert('a === b');
     } else if (a == b) {
-        alert('a==b')
+        alert('a==b');
+    } else if (a === NaN) {
+        alert('NaN :(')
+    } else {
+        alert('Nothing about ' + a + ' ' + b);
     }
-};
-console.log(fun(3, 5));
-console.log(fun(4, 4));
-console.log(fun('4', 4));
+}
+
+function fun() {
+    var a = +prompt('Input a'); //string ->number
+    var b = +prompt('Input b');
+    comparatorAB(a, b);
+}
 
 /*Create function which reads a string from prompt and prints to console ‘TRUE’ if it
     is a number and contains ‘0’ in the end. Try to call it from the button onclick event.*/
-
-
-let fun1 = function () {
-    let num = prompt('Input number', 'number');
-    let res = typeof (+num);
-
-    if (res === 'number' && num % 10 === 0) {
-        console.log('TRUE')
+function fun1() {
+    var value = prompt('Input number', 'number');
+    if (value === '') {
+        console.log('FALSE');
+        return;
+    }
+    var valueNum = +value;
+    if (valueNum === 0) {
+        console.log('TRUE');
+        return;
+    }
+    if (!valueNum) {
+        console.log('FALSE');
+    } else if (valueNum % 10 === 0) {
+        console.log('TRUE');
     } else console.log('FALSE')
 }
 
 /*
 Create a button, which shows alert with message n+1 with each click. First click
 should show 1, the second 2, then 3, etc...*/
+var k = 0;
 
-let fun2 = function () {
-    for (let i = 1; i <= 5; i++) {
-        alert(i);
-    }
+function fun2() {
+    k++;
+    alert(k);
 }
 
 /*
 Change the 3rd task and read initial value form prompt (1 is default if typed not
 number)*/
 
-let fun3 = function () {
-    let num = prompt('Input number under 5', '1');
-    /*let res = typeof (num);
-    if (res != 'number'){
-        num = 1;
-    } else*/
-        for (num; num <= 5; num++) {
-            alert(num);
+var p;
+function fun3() {
+    if (p === undefined) {
+        p = +prompt('Input number');
+        if (p) {
+            p = p + 1;
+            alert(p);
+        } else {
+            p = 1;
+            alert(p);
+        }
+    } else {
+        p = p+1;
+        alert(p);
     }
 }
